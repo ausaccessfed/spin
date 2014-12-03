1718561754ab86c497408fee4137ea1ddc568516
Russell Ianniello <russell.ianniello@aaf.edu.au>
2014-12-03 15:12:13 +1000

    Initial commit.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,27 @@
+require 'simplecov'
+
+require 'factory_girl_rails'
+require 'faker'
+
+Dir['./spec/support/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.disable_monkey_patching!
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.include FactoryGirl::Syntax::Methods
+
+  RSpec::Matchers.define_negated_matcher :not_change, :change
+end
