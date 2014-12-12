a6ed9a5236826e22511037da674ff5cfaeb1d068
Russell Ianniello <russell.ianniello@aaf.edu.au>
2014-12-12 10:50:28 +1000

    Updated tests to conform w/ latest rapidrack vers

--- spec/lib/authentication/subject_receiver_spec.rb
+++ spec/lib/authentication/subject_receiver_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 module Authentication
   RSpec.describe SubjectReceiver do
+    let(:env) { {} }
 
     context '#map_attributes' do
       let(:attrs) do
@@ -10,7 +11,7 @@ module Authentication
       end
 
       it 'maps the attributes' do
-        expect(subject.map_attributes(attrs))
+        expect(subject.map_attributes(env, attrs))
           .to eq(name: 'displayname',
                  mail: 'mail',
                  shared_token: 'auedupersonsharedtoken',
@@ -24,25 +25,25 @@ module Authentication
       end
 
       it 'creates the subject' do
-        expect { subject.subject(attrs) }.to change(Subject, :count).by(1)
+        expect { subject.subject(env, attrs) }.to change(Subject, :count).by(1)
       end
 
       it 'returns the new subject' do
-        obj = subject.subject(attrs)
+        obj = subject.subject(env, attrs)
         expect(obj).to be_a(Subject)
         expect(obj).to have_attributes(attrs)
       end
 
       it 'updates an existing subject' do
-        obj = subject.subject(attrs.merge(name: 'Wrong',
-                                          mail: 'wrong@example.com'))
-        subject.subject(attrs)
+        obj = subject.subject(env, attrs.merge(name: 'Wrong',
+                                               mail: 'wrong@example.com'))
+        subject.subject(env, attrs)
         expect(obj.reload).to have_attributes(attrs)
       end
 
       it 'returns the existing subject' do
-        obj = subject.subject(attrs)
-        expect(subject.subject(attrs)).to eq(obj)
+        obj = subject.subject(env, attrs)
+        expect(subject.subject(env, attrs)).to eq(obj)
       end
 
     end
