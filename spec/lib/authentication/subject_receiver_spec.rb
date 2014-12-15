6569f1b58f1ecb9ecfa8e70e2f9bbbea1aa8273c
Russell Ianniello <russell.ianniello@aaf.edu.au>
2014-12-16 09:42:11 +1000

    Renamed test subject to aid readability

--- spec/lib/authentication/subject_receiver_spec.rb
+++ spec/lib/authentication/subject_receiver_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 module Authentication
   RSpec.describe SubjectReceiver do
+    subject(:receiver) { SubjectReceiver.new }
     let(:env) { {} }
 
     context '#map_attributes' do
@@ -11,7 +12,7 @@ module Authentication
       end
 
       it 'maps the attributes' do
-        expect(subject.map_attributes(env, attrs))
+        expect(receiver.map_attributes(env, attrs))
           .to eq(name: 'displayname',
                  mail: 'mail',
                  shared_token: 'auedupersonsharedtoken',
@@ -25,25 +26,25 @@ module Authentication
       end
 
       it 'creates the subject' do
-        expect { subject.subject(env, attrs) }.to change(Subject, :count).by(1)
+        expect { receiver.subject(env, attrs) }.to change(Subject, :count).by(1)
       end
 
       it 'returns the new subject' do
-        obj = subject.subject(env, attrs)
+        obj = receiver.subject(env, attrs)
         expect(obj).to be_a(Subject)
         expect(obj).to have_attributes(attrs)
       end
 
       it 'updates an existing subject' do
-        obj = subject.subject(env, attrs.merge(name: Faker::Name.name,
-                                               mail: Faker::Internet.email))
-        subject.subject(env, attrs)
+        obj = receiver.subject(env, attrs.merge(name: Faker::Name.name,
+                                                mail: Faker::Internet.email))
+        receiver.subject(env, attrs)
         expect(obj.reload).to have_attributes(attrs)
       end
 
       it 'returns the existing subject' do
-        obj = subject.subject(env, attrs)
-        expect(subject.subject(env, attrs)).to eq(obj)
+        obj = receiver.subject(env, attrs)
+        expect(receiver.subject(env, attrs)).to eq(obj)
       end
     end
   end
