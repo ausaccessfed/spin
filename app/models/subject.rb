0022d272652a02832ee5a272d128c3a0244e29b3
Russell Ianniello <russell.ianniello@aaf.edu.au>
2014-12-17 09:50:40 +1000

    Moved include statement to top of class

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,10 +1,10 @@
 class Subject < ActiveRecord::Base
+  include Accession::Principal
+
   audited
   has_associated_audits
   has_many :subject_roles
   has_many :roles, through: :subject_roles
-
-  include Accession::Principal
 
   validates :name, :mail, presence: true
   validates :targeted_id, :shared_token, presence: true, if: :complete?
