ddfffe235517e83b95152f61d2d9b56ea9607376
Russell Ianniello <russell.ianniello@aaf.edu.au>
2014-12-22 09:58:45 +1000

    Added additional test for sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,17 @@ class SessionsController < ApplicationController
   def create
     # User has consented, time to authenticate...
     if (params['agree_to_consent'] == 'on')
-      Rails.logger.info('User has consented, redirecting to rapid auth')
-      env['rack.session'][:consent] = true
-      redirect_to('/auth/login')
-      return
+      set_user_consent
+      return redirect_to('/auth/login')
     end
 
     # We only get here if js validation never occurred.
     Rails.logger.info('User has NOT consented, redirecting to root')
     redirect_to root_url
+  end
+
+  def set_user_consent
+    Rails.logger.info('User has consented, setting in rack session')
+    env['rack.session'][:consent] = true if env['rack.session']
   end
 end
