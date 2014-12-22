0f13279a14d6fcd31cd537069ae49959ad8e809b
Russell Ianniello <russell.ianniello@aaf.edu.au>
2014-12-22 10:13:43 +1000

    Added "no_projects_assigned" page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,9 @@
 class ProjectsController < ApplicationController
   before_action :require_subject
+
   def index
+  end
+
+  def no_projects_assigned
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RapidRack::Engine => '/auth'
   get 'projects', to: 'projects#index'
+  get 'no_projects_assigned', to: 'projects#no_projects_assigned'
   post 'login', to: 'sessions#create'
   root to: 'welcome#index'
 end
