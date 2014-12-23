38c949b6a62a9831a7966171b1a5ea8934265ea7
Russell Ianniello <russell.ianniello@aaf.edu.au>
2014-12-24 09:33:33 +1000

    Replaced js validation with lipstick helper
    The welcome page consent check box is now validated using
    lipstick's validate_form helper.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,21 +2,7 @@
 // All this logic will automatically be available in application.js.
 
 $(document).ready(function () {
-
     // Initialise checkbox
     $('.checkbox').checkbox();
-
-    $('.ui.form')
-        .form({
-            terms: {
-                identifier: 'agree_to_consent',
-                rules: [
-                    {
-                        type: 'checked',
-                        prompt: 'You must agree to the terms and conditions'
-                    }
-                ]
-            }
-        });
 });
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
   include Lipstick::Helpers::LayoutHelper
   include Lipstick::Helpers::NavHelper
+  include Lipstick::Helpers::FormHelper
 end
