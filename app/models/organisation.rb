0a2aab3819605b3b6f5f0d4733f9034b338520a8
Russell Ianniello <russell.ianniello@aaf.edu.au>
2015-01-15 08:21:40 +1000

    Added cascading delete on AWS models

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,6 +1,6 @@
 class Organisation < ActiveRecord::Base
   audited
-  has_many :projects
+  has_many :projects, dependent: :destroy
 
   validates :name, :external_id, presence: true
   validates :external_id, uniqueness: true
--- app/models/project_role.rb
+++ app/models/project_role.rb
@@ -2,7 +2,7 @@ class ProjectRole < ActiveRecord::Base
   audited associated_with: :project
   has_associated_audits
 
-  has_many :subject_project_roles
+  has_many :subject_project_roles, dependent: :destroy
   has_many :subjects, through: :subject_project_roles
   belongs_to :project
 
