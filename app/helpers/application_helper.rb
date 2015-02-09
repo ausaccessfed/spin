3b2a3b3e47a0c1e89e62751abc4fa50af6727de6
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-02-10 08:30:42 +1000

    Bump version to alpha2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   include Lipstick::Helpers::NavHelper
   include Lipstick::Helpers::FormHelper
 
-  VERSION = '1.0.0-alpha'
+  VERSION = '1.0.0-alpha2'
 
   SUPPORT_MD = Rails.root.join('config/support.md').read
   SUPPORT_HTML = Kramdown::Document.new(SUPPORT_MD).to_html
