8d8a4cb4aef29d1b456dae1f9fa624853aaea935
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-03-10 15:19:44 +1000

    Integrate useful details into the SPIN dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
   def index
     public_action
+    @organisation_count = Organisation.count
+    @project_count = Project.count
+    @subject_count = Subject.count
   end
 end
